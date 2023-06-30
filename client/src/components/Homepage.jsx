ddc7f91049a8ba6fde9d5c511000ee8584cdeb9a
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-06-30 14:57:00 -0400

    Finished home page

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { useAppContext } from '../libs/context'
 
 const Homepage = () => {
@@ -15,10 +16,65 @@ const Homepage = () => {
 
                 {/* Bottom Block */}
                 <div className='block-bottom'>
-                    test
+                    {/* Logo */}
+                    <div id='home-logo' className='block-outline'>
+                        <img src="https://i.imgur.com/2YwsVn2.png" alt="" />
+                    </div>
+
+                    {/* Links */}
+                    <div id='home-links' className='block-outline'>
+                        {/* Top */}
+                        <p className='block-top bold'>Links</p>
+                        {/* Links Bottom */}
+                        <div id='home-links-bottom'>
+                            {/* Movies */}
+                            <Link to='/movies'>
+                                <p className='hl-link link-margin'>
+                                    Movies
+                                </p>
+                            </Link>
+                            {/* Users */}
+                            <Link to='/users'>
+                                <p className='hl-link link-margin'>
+                                    Users
+                                </p>
+                            </Link>
+
+                            {/* Conditional Items */}
+                            {
+                                !loggedUser ?
+                                    // If user is not logged in
+                                    <div>
+                                        {/* Login */}
+                                        < Link to='/login'>
+                                            <p className='hl-link'>
+                                                Login
+                                            </p>
+                                        </Link>
+                                    </div>
+                                    : 
+                                    // If user is logged in
+                                    <div>
+                                    {/* My Reviews */}
+                                    < Link to={`/users/${loggedUser._id}`}>
+                                        <p className='hl-link link-margin'>
+                                            My Reviews
+                                        </p>
+                                    </Link>
+                                    {/* Watchlist */}
+                                    < Link to={`/watchlist/`}>
+                                        <p className='hl-link'>
+                                            Watchlist
+                                        </p>
+                                    </Link>
+                                </div>
+                            }
+
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
